3af81fe50a484d5e3527787b3e1dbdc483e93723
Farid Fox <github@faridfox.com>
2025-06-02 20:32:22 +0300

    Added scripts file

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+// Initialize Swiper
+var mySwiper = new Swiper(".swiper-container", {
+  direction: "vertical",
+  loop: true,
+  centeredSlides: true,
+  speed: 9000,
+  autoplay: {
+    delay: 1,
+  },
+  slidesPerView: "auto",
+  allowTouchMove: false,
+  disableOnInteraction: true,
+});
+var mySwiper = new Swiper(".swiper-container1", {
+  direction: "vertical",
+  loop: true,
+  speed: 9000,
+  autoplay: {
+    delay: 2,
+  },
+  slidesPerView: "auto",
+  allowTouchMove: false,
+  disableOnInteraction: true,
+});
